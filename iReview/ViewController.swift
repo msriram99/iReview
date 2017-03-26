98a4a3354468dd9653881bba3d29440b6a8f81f7
Himaja Motheram <himaja2@gmail.com>
2017-03-26 19:03:47 -0400

    iReview101

--- iReview/ViewController.swift
+++ iReview/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var restuarantNameLabel        :UILabel!
+    @IBOutlet var yourNameTextField :UITextField!
+    @IBOutlet var yourReviewTextView        :UITextView!
+    @IBOutlet var recommendSwitch    :UISwitch!
+    @IBOutlet var overallRatingSlider    :UISlider!
+    @IBOutlet var foodRatingSegControl  :UISegmentedControl!
+    
+    @IBAction func submitButtonPressed(button: UIButton) {
+        guard let name = yourNameTextField.text else {
+            return
+        }
+        restuarantNameLabel .text = "\(name) Favorite Restaurant"
+          print("Customer Name: \(yourNameTextField .text)")
+          print("Review Text: \(yourReviewTextView.text)")
+          print("Overall Rating: \(overallRatingSlider.value)")
+          print("Food Rating: \(foodRatingSegControl.selectedSegmentIndex)")
+          print("Recommended: \(recommendSwitch.isOn)")
+       
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //yourNameTextField.delegate = self
+        recommendSwitch.isOn = false
+        
+        overallRatingSlider.value = 0.35
+        foodRatingSegControl.selectedSegmentIndex = 0
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
